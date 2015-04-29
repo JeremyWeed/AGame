f7c0ac89134486c1f8329b5635e12cb20772772b
JeremyWeed <jweed262@gmail.com>
2015-04-28 22:39:35 -0400

    things may or may not have happened

---
+++ src/Projectiles/Projectile.java
@@ -0,0 +1,12 @@
+package Projectiles;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Jeremy on 3/3/2015.
+ */
+public abstract class Projectile {
+    BufferedImage img;
+    String imgLoc;
+
+}
